c922684e509ba86e8a9d7d9378e4917b85404bb5
GHSyrine <161436600+GHSyrine@users.noreply.github.com>
2024-05-14 16:30:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// index.php
+
+require_once 'Car.php';
+require_once 'Vehicle.php';
+
+$car = new Car('green', 4, 'electric');
+try{
+    $car->start();
+}catch (Exception $e){
+    echo "Exception  : " . $e->getMessage() . "\n";
+    $car->setHasParkBrake(false);
+    echo "Frein à main désactivé.\n";
+} finally {
+    echo "Ma voiture roule comme un donut.\n";
+}
